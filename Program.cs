c16d0fd82ec7638f1d2b421670449791e3f83a26
MashaJing <53327043+MashaJing@users.noreply.github.com>
2021-05-14 17:26:28 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Numerics;
+using System.IO;
+using ExcelDataReader;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAND_1.X2_method;
+
+namespace SAND_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //открываем excel файл и строим двумерный массив
+            //результат чтения
+            double[][] RES = new double[11][];
+
+            //чтение данных из excel-файла: потоку указываем путь, метод открытия файла, уровень доступа  
+            using (var stream = File.Open(Path.Combine("C:\\Users\\mdzek\\source\\repos\\SAND_1\\SAND_1\\Excel_Data\\Arrays.xlsx"), FileMode.Open, FileAccess.Read))
+            {
+                //читатель для файлов с расширением *.xlsx
+                var excelReader = ExcelReaderFactory.CreateReader(stream);
+
+               //инициализируем хранилище элементов
+                for (int i = 0; i < 11; i++)
+                {
+                    RES[i] = new double[1000];
+                    //считываем очередную строку из книги Excel
+                    excelReader.Read();
+
+                    //цикл по столбцам книги
+                    for (int j = 0; j < 1000; j++) 
+                    { RES[i][j] = excelReader.GetDouble(j); }
+                }
+
+                //после завершения чтения освобождаем ресурсы
+                excelReader.Close();
+            }
+
+            Console.WriteLine("Adm_Dur\t | ProductRel\t | TrafficT\t | Month\t | Weekend\t | OS\t | ProductRel_Dur\t | Info_Dur\t | SpecialDay\t | BounceRates\t | Revenue\t");
+            //вызываем функцию для вычисления парной взаимосвязи
+
+            Xi2_check check = new Xi2_check();
+
+            double[][] Kramer_arr = new double[10][];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Kramer_arr[i] = new double[10];
+                for (int j = 0; j < 10; j++)
+                {
+                    Kramer_arr[i][j] = new double();
+                    if (j == i)
+                        Kramer_arr[i][j] = 0; //чтобы не учитывались при вычислении F(S)
+                    else
+                    {
+                        Kramer_arr[i][j] = check.Count_Xi2(RES[j], RES[i]);
+                    }
+                    Console.Write("{0}\t", Math.Round(Kramer_arr[i][j], 6));
+                }
+                Console.WriteLine();
+            }
+
+            double[] Rev_Kramer_arr = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Rev_Kramer_arr[i] = new double();
+                Rev_Kramer_arr[i] = check.Count_Xi2(RES[i], RES[10]);
+            }
+
+            int iter = 1;
+            double r = 0;
+            double R = 0;
+            int one_counter = 0;
+            List<int> Res;
+            double[] FS = new double[1023];
+
+            while (iter < 1024)
+            {
+                Res = into_binary(iter);
+                Console.WriteLine(Res.Count());
+
+                for (int i=0; i<10; i++)
+                {
+                    R += Rev_Kramer_arr[i] * Res[i];
+                    one_counter += Res[i];
+                }
+                for (int j = 0; j < 10; j++)
+                {
+                    if (Res[j] == 0) continue;
+
+                    for (int i = 0; i < 10; i++)
+                        r += Kramer_arr[j][i] * Res[i];
+                }
+                FS[iter-1] = R/Math.Sqrt(one_counter + r);
+                
+                
+                R = 0;
+                r = 0;
+                one_counter = 0;
+                iter++;
+            }
+
+            double max_elem = 0;
+            iter = 0;
+            for (int i = 0; i < FS.Length; i++)
+            {
+                if (FS[i] > max_elem) 
+                {
+                    max_elem = FS[i];
+                    iter = i;
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", into_binary(iter + 1).ToArray()));
+            Console.Read();
+
+  //          Console.WriteLine();
+
+  //          Console.WriteLine("Pirson:");
+  //          for (int i = 10; i > 0; i--)
+  //          {
+  //              for (int j = 0; j < 10; j++)
+  //              {
+  //                  if (j >= i)
+  //                      Console.Write(" -\t\t");
+  //                  else
+  //                  {
+  //                      double Xi = check.Count_Xi2(RES[j], RES[i]);
+  ////                      Console.Write("{0}\t", Math.Round(Xi2_check.Count_Pirson_ratio(Xi, 1000), 6));
+  //                  }
+
+  //              }
+  //              Console.WriteLine();
+  //          }
+
+            /*
+            //приложение, которое откроет excel-файл по завершению алгоритма программы
+            Excel.Application Excel_App = new Excel.Application();
+
+            //создаём экземпляр рабочей книги Excel
+            Excel.Workbook Work_Book = Excel_App.Workbooks.Add();
+
+            //создаём экземпляр листа Excel: 1 - номер листа из списка листов (если лист будет не один в книге)
+            Excel.Worksheet Work_Sheet = (Excel.Worksheet)Work_Book.Worksheets.get_Item(1);
+
+            //указываем имя листа книги
+            Work_Sheet.Name = "Correlations";
+
+            //заполняем строки таблицы числами
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int j = 2; j <= 1000; j++)
+                {
+                    Work_Sheet.Cells[j][i] = RES[i - 1][j - 1];
+                }
+            }
+
+            //открываем созданный excel-файл
+            Excel_App.Visible = true;
+            Excel_App.UserControl = true;
+            */
+        }
+
+        public static List<int> into_binary(int a)
+        {
+            List<int> result = new List<int>();
+
+            if (a == 0)
+            {
+                while (result.Count() < 10) result.Add(0);
+            }
+            else
+            {
+                while (a != 1)
+                {
+                    result.Add(a % 2);
+                    a /= 2;
+                }
+                result.Add(1);
+                while (result.Count() < 10) result.Add(0);
+                result.Reverse();
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ X2_method/Xi2_check.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAND_1.X2_method
+{
+    class Xi2_check
+    {
+        //для alpha = 0.05 и n = [1, 16] приведены значения (1-alpha)-квантили X^2-распределния
+        Dictionary<int, double> Xi2_quantile = new Dictionary<int, double> { 
+    {1, 3.841},
+    {2, 5.991},
+    {3, 7.815},
+    {4, 9.488},
+    {5, 11.07},
+    {6, 12.592},
+    {7, 14.067},
+    {8, 15.507},
+    {9, 16.919},
+    {10, 18.307},
+    {11, 19.675},
+    {12, 21.026},
+    {13, 22.362},
+    {14, 23.685},
+    {15, 24.996},
+    {16, 26.296},
+    {20, 10.85},
+    {24, 13.848},
+    {25, 14.611},
+    {27, 16.151},
+    {36, 23.268},
+    {45, 30.612},
+    {60, 43.188},
+    {72, 53.462},
+    {75, 56.054},
+    {84, 63.876},
+    {135, 109.156}
+    };
+
+
+        public double Count_Xi2(double[] ExplainV, double[] dependV)
+        {
+            //Составляем массив неповторяющихся величин
+            IEnumerable<double> ExplainVDist = ExplainV.Distinct();
+            IEnumerable<double> DependVDist = dependV.Distinct();
+
+            //создаем массивы сумм частот по строкам и столбцам
+            int[] Sumi = new int[ExplainVDist.Count()];
+            int[] Sumj = new int[DependVDist.Count()];
+
+            //составляем таблицу частот
+            int[][] TableFreq = new int[Sumi.Length][];
+
+            int col, row = 0;
+
+            foreach (double val in ExplainVDist)
+            {
+                TableFreq[row] = new int[Sumj.Length];
+                for (int i = 0; i < ExplainV.Length; i++) //проходимся по всему списку размером 1000
+                {
+                        col = 0;
+                    foreach (double category in DependVDist) //проверяем частоту по одному значению зависимой переменной
+                    {
+                       if (dependV[i] == category && ExplainV[i] == val)
+                         { 
+                            TableFreq[row][col]++;
+                            break;
+                         }
+                        col++;
+                    }
+                }
+                row++;
+            }
+
+            //инициализируем массивы сумм 
+            for (int i = 0; i < Sumi.Length; i++)
+            {
+                for (int j = 0; j < Sumj.Length; j++)
+                {
+                    Sumi[i] += TableFreq[i][j];
+                    Sumj[j] += TableFreq[i][j];
+                }
+            }
+
+            //считаем статистику хи-квадрат
+            double Xi2 = 0;
+            double ExpecFreq;
+
+            for (int i = 0; i < Sumi.Length; i++)
+                for (int j = 0; j < Sumj.Length; j++)
+                {
+                    ExpecFreq = ((double)Sumi[i]*Sumj[j])/1000;
+                    Xi2 += Math.Pow(TableFreq[i][j] - ExpecFreq, 2)/ ExpecFreq;
+                }
+
+            //Console.Write("{0}\t", Math.Round(Count_Kramer_ratio(Xi2, 1000, Sumi.Length, Sumj.Length), 5));
+            //Is_dependent(Xi2, (Sumi.Length - 1) * (Sumj.Length - 1));
+            //return Xi2;
+            return Count_Kramer_ratio(Xi2, 1000, Sumi.Length, Sumj.Length);
+        }
+
+        public static double Count_Kramer_ratio(double Xi2, int n, int a, int b)
+        {
+            return Math.Sqrt(Xi2 / n/Math.Min(a-1,b-1));
+        }
+
+        public static double Count_Pirson_ratio(double Xi2, int n)
+        {
+            return Math.Sqrt(Xi2 /(n + Xi2));
+        }
+
+        public bool Is_dependent(double Xi2, int n)
+        {
+            return Xi2 > Xi2_quantile[n];
+        }
+
+    }
+}
